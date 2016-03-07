bd75c7a658824c2d5bff4ffb506dd8ad20e2fffd
Will Coster <willcoster@gmail.com>
2016-03-06 20:57:40 -0800

    Add initial version of the web app
    The current version of the web app just provides a handful of fixed
    graphs and a way to download a copy of the log file.

---
+++ README.md
@@ -0,0 +1,15 @@
+IRC Stats
+=========
+
+Monolithic #GreatestGuys IRC logs dating back to February 2013. Logs are
+available in a easy to process JSON format and regenerated hourly. Add new
+graphs and visualizations by forking on GitHub. New templates will be
+automatically pulled in hourly.
+
+Web App
+-------
+
+The web app assumes that you have a copy of the log file in the root of your git
+checkout.
+
+To add new graphs to the home page edit the `web/templates/index.html` file.
---
+++ web/stats.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import datetime
+import json
+import os
+import re
+
+from flask import Flask, url_for, render_template, g
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_STATIC = os.path.join(APP_ROOT, 'static')
+
+DEBUG = True
+
+logs = None
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+query_cache = {}
+def query_logs(s, cummulative=False):
+    """
+    Query logs for a given regular expression and return a time series of the
+    number of occurrences of lines matching the regular expression per day.
+    """
+    global query_cache
+
+    cache_key = (s, cummulative)
+    if cache_key in query_cache:
+        return query_cache[cache_key]
+
+    r = re.compile(s)
+    results = {}
+
+    total = 0
+
+    def get_key(timestamp):
+        d = datetime.datetime.fromtimestamp(float(timestamp))
+        return time.mktime(
+                datetime.datetime(d.year, d.month, d.day).timetuple())
+
+    def get_value(key):
+        if key not in results:
+            results[key] = {'x': key, 'y': 0}
+        return results[key]
+
+    for line in logs:
+        if r.search(line['message']) == None:
+            continue
+        key = get_key(line['timestamp'])
+        value = get_value(key)
+        total += 1
+
+        if cummulative:
+            value['y'] = total
+        else:
+            value['y'] += 1
+
+    values = results.values()
+    values.sort(key=lambda x: x['x'])
+
+    # Cache the result of the query so that later queries with the same
+    # parameters are fast. Also, to prevent OOM clear the cache once it gets too
+    # big.
+    if len(query_cache) > 1000:
+        query_cache = {}
+    query_cache[cache_key] = values
+
+    return values
+
+def graph_query(queries, cummulative=False):
+    data = []
+    for (label, s) in queries:
+        data.append({
+            'key': label,
+            'values': query_logs(s, cummulative=cummulative),
+        })
+    return data
+
+app.jinja_env.globals['graph_query'] = graph_query
+
+@app.before_request
+def misc_stats():
+    # Cache the entire log file since it takes several seconds to parse.
+    global logs
+    if not logs:
+        with open(os.path.join(APP_STATIC, 'log.json'), 'r') as f:
+            logs = json.load(f)
+
+    g.logs = logs
+    g.num_tnaks = len(filter(lambda m: 'tnak' in m['message'].lower(), logs))
+
+@app.route('/')
+def show_entries():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
