93be16ba68bc4131f5b54a5dd6ac75b66ef8a985
Will Coster <willcoster@gmail.com>
2016-03-27 18:24:12 -0700

    Use multiple threads

--- runserver.py
+++ runserver.py
@@ -4,4 +4,4 @@ import os
 from web import app
 
 port = 'PORT' in os.environ and int(os.environ['PORT']) or 5000
-app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port, threaded=True)
